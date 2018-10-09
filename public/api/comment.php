14464403a22391519a39709c4f87bb8c5289ba67
danielkilcullen <dkilcull@iu.edu>
2018-10-09 15:38:20 -0400

    almost working (post)

--- public/api/comment.php
+++ public/api/comment.php
@@ -4,9 +4,6 @@ require '../../app/common.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   require 'commentPost.php';
-  $comment = new Comment($_POST);
-  $comment->create();
-  echo json_encode($comment);
   exit;
 }
 
