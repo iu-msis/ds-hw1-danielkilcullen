5a366470ddb28af58fbb638a968e2fd8d17f86b3
danielkilcullen <dkilcull@iu.edu>
2018-10-16 19:51:06 -0400

    took out console.log

--- public/js/comments.app.js
+++ public/js/comments.app.js
@@ -10,10 +10,9 @@ var commentsApp = new Vue({
   },
   methods: {
     fetchComments: function() {
-      fetch('ec2-18-219-100-144.us-east-2.compute.amazonaws.com/api/comment.php')
+      fetch('../api/comment.php')
       .then( response => response.json() )
       .then( json => { this.commentArr = json } )
-      .then(console.log(json)) //testing
       .catch( err => {
         console.log("COMMENT FETCH error: ");
         console.log(err);
