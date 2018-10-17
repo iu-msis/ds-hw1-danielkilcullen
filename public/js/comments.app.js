f1fd02955f76f3f5b57899a125e669692022bb17
danielkilcullen <dkilcull@iu.edu>
2018-10-16 20:45:05 -0400

    a shot in the dark

--- public/js/comments.app.js
+++ public/js/comments.app.js
@@ -17,9 +17,41 @@ var commentsApp = new Vue({
         console.log("COMMENT FETCH error: ");
         console.log(err);
       })
+    },
+    getEmptyCommentForm: function() {
+      return {
+        // comment: '';
+      }
+    },
+    handleCommentForm: function() {
+      if (this.commentForm == null) {
+              console.error("Can't submit (it's null)");
+              return;
+      }
+
+      const s = JSON.stringify(this.commentForm);
+      console.log(s);//testing
+
+      fetch('../api/comment.php', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json; charset=utf-8"
+        },
+        body: s
+      })
+      .then( response => response.json() )
+      .then( json => {this.commentArr.push(json)})
+      .catch( err => {
+        console.error('WORK POST ERROR:');
+        console.error(err);
+      })
+
+      this.commentForm = this.getEmptyCommentForm();
+    }
     }
   },
-  mounted() {
+  created() {
     this.fetchComments();
+    this.getEmptyCommentForm();
   }
 });
